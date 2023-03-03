0cc25a961644da8145507b2a9d4349fd01785efe
Armin Hrbat <92021852+arminhrbat@users.noreply.github.com>
2023-03-03 11:56:02 +0100

    testing

--- index.php
+++ index.php
@@ -1,13 +1,35 @@
 <?php
 
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 require 'vendor/autoload.php';
 
-Flight::route('/', function()
-{
-  echo "Hello world";
+Flight::route('/', function(){
+    // Set database connection parameters
+    $host = 'sql.freedb.tech';
+    $port = '3306';
+    $dbname = 'freedb_zmijugashop';
+    $username = 'freedb_arminhrbat';
+    $password = '*czPUfE%cND#y98';
+
+    // Connect to the database using PDO
+    $dsn = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4";
+    $pdo = new PDO($dsn, $username, $password);
+
+    // Prepare and execute a query to retrieve users' name and lastname
+    $stmt = $pdo->prepare('SELECT name, lastname FROM korisnici');
+    $stmt->execute();
+
+    // Fetch the results and store them in a variable
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    // Display the results
+    foreach ($results as $row) {
+        echo $row['name'] . ' ' . $row['lastname'] . '<br>';
+    }
 });
 
 Flight::start();
 
-
- ?>
+?>
